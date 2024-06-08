f100978d83264eba205f42dfe4b3e65fba5f0672
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-08 10:43:59 +0330

    changed Card header file

--- Card.hpp
+++ Card.hpp
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-=======
 #ifndef CARD_HPP
 #define CARD_HPP
->>>>>>> 10a37b3c6165781a99725dc5e57003351a18dfa0
 #include <string>
 class Card
 {
 public:
-<<<<<<< HEAD
     int getCharacter();
     int getNumberOfCard();
     void setCharacter(int value);
-=======
     Card(std::string nameOfCard);
     int getCharacter();
     int getNumberOfCard();
@@ -20,7 +15,6 @@ public:
     void setExplanation(std::string description);
     std::string getName();
     void setName(std::string nameOfCard);
->>>>>>> 10a37b3c6165781a99725dc5e57003351a18dfa0
 
 private:
     int numberOfCard;
@@ -28,11 +22,7 @@ private:
     int score;
     std::string typeOfCard;
     std::string nameOfCard;
-<<<<<<< HEAD
     std::string explanation;
-};
-=======
     std::string description;
 };
 #endif
->>>>>>> 10a37b3c6165781a99725dc5e57003351a18dfa0
