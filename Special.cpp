600319a6c5cd6144714183a610e666213a744140
Marzieh Shooshtari <marziehshooshtari@gmail.com>
2024-06-09 17:58:47 +0330

    special card added

--- Special.cpp
+++ Special.cpp
@@ -1,12 +1,5 @@
 #include "Special.hpp"
-Special::Special(std::string name) : Card (name)
-{
-}
-void Special::setName(std::string name)
-{
-    Card::setName(name);
-}
-void Special::setNumberOfCard(int numberOfCard)
-{
-    Card::setNumberOfCard(numberOfCard);
-}
+// Special::Special(std::string name) : Card (name)
+// {
+// }
+
--- Special.hpp
+++ Special.hpp
@@ -6,13 +6,11 @@ class Special : public Card
 {
 private:
 public:
-    Special(std::string name);
+    // Special(std::string name);
     void setCharacter(int value) override
     {
         Card::setCharacter(value);
     }
-    void setNumberOfCard(int value);
-    void setName(std::string name);
     // int getPriority();
 };
 #endif
