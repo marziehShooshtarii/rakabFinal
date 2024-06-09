5a452d2bee294485450be964eb760a26061d1e39
Marzieh Shooshtari <marziehshooshtari@gmail.com>
2024-06-09 17:14:20 +0330

    army override added

--- Army.cpp
+++ Army.cpp
@@ -1,23 +1,13 @@
 #include "Army.hpp"
 armyCard::armyCard(std::string name) : Card (name){};
 
-void armyCard::setCharacter (int value)
-{
-    Card::setCharacter(value);
-}
-int armyCard::getScore()
-{
-    return getCharacter();
-}
-void armyCard::SetNumberOfCard ()
-{
-    if (getScore () == 1)
-        Card::setNumberOfCard (10);
-    else 
-        Card::setNumberOfCard(8);    
-}
-void armyCard::setName(std::string name)
-{
-    Card{name};
-}
+
+
+// void armyCard::SetNumberOfCard ()
+// {
+//     if (getScore () == 1)
+//         Card::setNumberOfCard (10);
+//     else 
+//         Card::setNumberOfCard(8);    
+// }
 
--- Army.hpp
+++ Army.hpp
@@ -4,13 +4,15 @@
 class armyCard : public Card
 {
 public:
-void setCharacter ();
-int getScore();
     armyCard(std::string name);
-    void setCharacter(int value);
-    int getScore();
-    void SetNumberOfCard();
-    void setName (std::string name);
-
+    void setCharacter(int value) override
+    {
+        Card::setCharacter(value);
+    }
+    int getCharacter() override
+    {
+        return Card::getCharacter();
+    }
+    //    void SetNumberOfCard();
 };
 #endif
