00d1426ee6521dd684e7d93db1ac24e302aa5b56
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-14 00:14:20 +0330

    card updated

--- Card.cpp
+++ Card.cpp
@@ -1,6 +1,7 @@
-#include "Card.hpp"
 #include <string>
-Card::Card(){}
+#include <iostream>
+#include "Card.hpp"
+Card::Card() {}
 Card::Card(const std::string &p_nameOfCard)
 {
     nameOfCard = p_nameOfCard;
@@ -16,10 +17,87 @@ void Card::setExplanation(std::string p_description)
 }
 void Card::setName(std::string p_name)
 {
-    this ->nameOfCard = p_name; 
+    this->nameOfCard = p_name;
 }
-bool Card::operator == (const Card &c){ 
-            if (nameOfCard == c.nameOfCard ) 
-                return true; 
-            return false; 
-        } 
+bool Card::operator==(const Card &c)
+{
+    if (nameOfCard == c.nameOfCard)
+        return true;
+    return false;
+}
+// void Card::setCardScores()
+// {
+//     cardsScore =
+//         {
+//             {"bahar", 0},
+//             {"zemestan", 0},
+//             {"matarsak", 0},
+//             {"tabl_zan", 0},
+//             {"shah_dokht", 0},
+//             {"sarbaz_1", 1},
+//             {"sarbaz_2", 2},
+//             {"sarbaz_3", 3},
+//             {"sarbaz_4", 4},
+//             {"sarbaz_5", 5},
+//             {"sarbaz_6", 6},
+//             {"sarbaz_10", 10},
+//         };
+// }
+// int Card::searchInCardScore(std::string str)
+// {
+//     return cardsScore.at(str);
+// }
+// void Card::kooft(int maxscore)
+// {
+//     for (auto it = cardsScore.begin(); it != cardsScore.end(); it++)
+//     {
+//         if (it == maxscore)
+//         {
+//             std::cout << it->first;
+//         }
+//     }
+// }
+void Card::setCardScores()
+{
+    cardsScore =
+        {
+            {"bahar", 0},
+            {"zemestan", 0},
+            {"matarsak", 0},
+            {"tabl_zan", 0},
+            {"shah_dokht", 0},
+            {"sarbaz_1", 1},
+            {"sarbaz_2", 2},
+            {"sarbaz_3", 3},
+            {"sarbaz_4", 4},
+            {"sarbaz_5", 5},
+            {"sarbaz_6", 6},
+            {"sarbaz_10", 10},
+        };
+}
+int Card::searchInCardScore(std::string str)
+{
+    return cardsScore.at(str);
+}
+// void Card::setStringToSpecialCard()
+// {
+//     stringToSpecialCard =
+//         {
+//             {"bahar", 0},
+//             {"zemestan", specialCards[0]},
+//             {"matarsak", 0},
+//             {"tabl_zan", 0},
+//             {"shah_dokht", 0},
+//             {"sarbaz_1", 1},
+//             {"sarbaz_2", 2},
+//             {"sarbaz_3", 3},
+//             {"sarbaz_4", 4},
+//             {"sarbaz_5", 5},
+//             {"sarbaz_6", 6},
+//             {"sarbaz_10", 10},
+//         };
+// }
+// void Card::setSpacial()
+// {
+    
+// }
--- Card.hpp
+++ Card.hpp
@@ -1,11 +1,12 @@
 #ifndef CARD_HPP
 #define CARD_HPP
 #include <string>
+#include <unordered_map>
 class Card
 {
 public:
-    Card ();
-    Card(const std::string& p_nameOfCard);
+    Card();
+    Card(const std::string &p_nameOfCard);
     virtual int getCharacter()
     {
         return characteristic;
@@ -17,15 +18,25 @@ public:
     void setExplanation(std::string p_description);
     std::string getName();
     void setName(std::string p_name);
-    bool operator == (const Card &c);
+    bool operator==(const Card &c);
+
+    // int searchInCardScore(std::string str);
+    void kooft(int i);
+    std ::unordered_map<std::string, int> cardsScore;
+    void setCardScores();
+    int searchInCardScore(std::string str);
+    //std ::unordered_map<std::string, Card> stringToSpecialCard;
+    //void setStringToSpecialCard();
+   // void setSpacial();
+
 private:
-    int numberOfCard;
+    // int numberOfCard;
     int characteristic;
-    int score;
+    // int score;
     std::string typeOfCard;
     std::string nameOfCard;
     std::string explanation;
     std::string description;
-
+   // std::vector<Special> special;
 };
 #endif
