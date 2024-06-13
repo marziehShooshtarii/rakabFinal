a3ccd9d781c3ede29be8b4e6214915405430c025
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-13 18:22:43 +0330

    zemestan added

---
+++ Zemestan.hpp
@@ -0,0 +1,19 @@
+#ifndef ZEMESTAN_HPP
+#define ZEMESTAN_HPP
+#include "Special.hpp"
+class Zemestan : public Special
+{
+public:
+    virtual int effectOfCard(int score) override 
+    {
+        return score+=1;
+    }
+
+};
+
+
+
+
+
+
+#endif
