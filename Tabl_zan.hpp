cf0b151829ea2cf325396eefcc8d951aa7012142
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-13 18:25:10 +0330

    Tabl_zan added

---
+++ Tabl_zan.hpp
@@ -0,0 +1,19 @@
+#ifndef TABL_ZAN_HPP
+#define TABL_ZAN_HPP
+#include "Special.hpp"
+class Tabl_zan : public Special
+{
+public:
+    virtual int effectOfCard(int score) override 
+    {
+        return score*=2;
+    }
+
+};
+
+
+
+
+
+
+#endif
