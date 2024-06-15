04c7b719a2179d890d852cd05fa32e8a5b768686
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-15 17:54:29 +0330

    map fixed

--- Map.cpp
+++ Map.cpp
@@ -1,32 +1,63 @@
-#include <fstream>
+
 #include <iostream>
 #include "Map.hpp"
-bool Map::setMapStates()
-{
-    readMapFile.open("matrix.txt", std::ios::in);
-    if (!readMapFile.is_open())
-    {
-        std ::cerr << "Error opening the file!" << std::endl;
-        return 1;
-    }
-    for (int i = 0; i < 14; i++)
-    {
-        for (int j = 0; j < 14; j++)
-        {
-            readMapFile >> neighborMap[i][j];
-        }
-    }
-    for (int i = 0; i < 14; i++)
-    {
-        for (int j = 0; j < 14; j++)
-        {
-            std::cout << neighborMap[i][j] <<std::endl;
-        }
-    }
-    return 0;
-}
-int main (){
-    Map map;
-    map.setMapStates();
-    return 0;
-}
+// bool Map::setMapStates()
+// {
+//     readMapFile.open("matrix.txt", std::ios::in);
+//     if (!readMapFile.is_open())
+//     {
+//         std ::cerr << "Error opening the file!" << std::endl;
+//         return 1;
+//     }
+//     for (int i = 0; i < 14; i++)
+//     {
+//         for (int j = 0; j < 14; j++)
+//         {
+//             readMapFile >> neighborMap[i][j];
+//         }
+//     }
+//     for (int i = 0; i < 14; i++)
+//     {
+//         for (int j = 0; j < 14; j++)
+//         {
+//             std::cout << neighborMap[i][j] <<std::endl;
+//         }
+//     }
+//     return 0;
+// }
+// // int main (){
+// //     Map map;
+// //     map.setMapStates();
+// //     return 0;
+// // }
+// void Map::initializeStates()
+// {
+//     states = 
+//     {
+//         {"bella",0},
+//         {"caline",1},
+//         {"enna",2},
+//         {"atela",3},
+//         {"dimase",4},
+//         {"olivadi",5},
+//         {"lia",6},
+//         {"armento",7},
+//         {"morina",8},
+//         {"talmone",9},
+//         {"elinia",10},
+//         {"rollo",11},
+//         {"pladaci",12},
+//         {"borge",13},
+
+//     };
+
+
+//}
+// int Map::searchInStates(std::string str)
+// {
+//    return states.at(str);
+// }
+// int Map::getNeighborMap(int i,int j)
+// {
+//     return neighborMap[i][j];
+// }
--- Map.hpp
+++ Map.hpp
@@ -1,35 +1,79 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 #include <fstream>
+#include <unordered_map>
+#include <iostream>
 class Map
 {
 public:
-    bool setMapStates();
-    // {
-    //     for (int i = 0; i < 14; i++)
-    //     {
-    //         neighborMap [i][i] = 0;
-    //     }
-
-    // }
-private:
-    enum states
+    // bool setMapStates();
+    //void initializeStates();
+    //int searchInStates(std::string);
+    //int getNeighborMap(int i, int j);
+    bool setMapStates()
+{
+    readMapFile.open("matrix.txt", std::ios::in);
+    if (!readMapFile.is_open())
     {
-        BELLA,
-        CALIE,
-        ENNA,
-        ATELA,
-        DIMASE,
-        OLIVADI,
-        LIA,
-        ARMENTO,
-        MORINA,
-        TALMONE,
-        ELINIA,
-        ROLLO,
-        PLADACI,
-        BORGE
+        std ::cerr << "Error opening the file!" << std::endl;
+        return 1;
+    }
+    for (int i = 0; i < 14; i++)
+    {
+        for (int j = 0; j < 14; j++)
+        {
+            readMapFile >> neighborMap[i][j];
+        }
+    }
+    // for (int i = 0; i < 14; i++)
+    // {
+    //     for (int j = 0; j < 14; j++)
+    //     {
+    //         std::cout << neighborMap[i][j] <<std::endl;
+    //     }
+    // }
+    return 0;
+}
+// int main (){
+//     Map map;
+//     map.setMapStates();
+//     return 0;
+// }
+void initializeStates()
+{
+    states = 
+    {
+        {"bella",0},
+        {"caline",1},
+        {"enna",2},
+        {"atela",3},
+        {"dimase",4},
+        {"olivadi",5},
+        {"lia",6},
+        {"armento",7},
+        {"morina",8},
+        {"talmone",9},
+        {"elinia",10},
+        {"rollo",11},
+        {"pladaci",12},
+        {"borge",13},
+
     };
+
+
+}
+int searchInStates(std::string str)
+{
+   return states.at(str);
+}
+int getNeighborMap(int i,int j)
+{
+    return neighborMap[i][j];
+}
+
+private:
+std::unordered_map<std::string,int>states;
+
     bool neighborMap[14][14];
     std ::ifstream readMapFile;
     // Player owner;
