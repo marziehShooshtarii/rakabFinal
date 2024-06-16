c7d47589d53fdd2573710709f34682bff1190ff1
paniz <paniz84yaghoobzadeh@gmail.com>
2024-06-16 18:56:13 +0330

    shir dokht changed to shah dokht

---
+++ Shah_dokht.hpp
@@ -0,0 +1,19 @@
+#ifndef SHAH_DOKHT_HPP
+#define SHAH_DOKHT_HPP
+#include "Special.hpp"
+class Shah_dokht : public Special
+{
+public:
+    virtual int effectOfCard(int score) override 
+    {
+        return score+=10;
+    }
+
+};
+
+
+
+
+
+
+#endif
