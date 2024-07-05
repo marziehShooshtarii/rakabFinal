6cb6c2d84e85a27bb43def4703d000784bdaf82c
paniz <paniz84yaghoobzadeh@gmail.com>
2024-07-06 00:52:48 +0330

    army redone with polymorphysm

--- Army.cpp
+++ Army.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "Army.hpp"
-int armyCard::increasScore(std::string str, int score)
+int Army::increasScore(std::string str, int score)
 {
     setCardScores();
     return (score + cardsScore.at(str));
+}
+int Army::effectOfCard(std::vector<Card> playedCards, int score)
+{
+    for (int i = 0; i < playedCards.size(); i++)
+    {
+        score = score + cardsScore.at(playedCards[i].getName());
+    }
+    return score;
 }
--- Army.hpp
+++ Army.hpp
@@ -1,9 +1,10 @@
 #ifndef ARMY_HPP
 #define ARMY_HPP
 #include "Card.hpp"
-class armyCard : public Card
+class Army : public Card
 {
 public:
     int increasScore(std::string, int score);
+    virtual int effectOfCard(std::vector<Card>,int score) override;
 };
 #endif
