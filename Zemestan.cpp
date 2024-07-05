dd0869b8c82146d54525e56b1a9b7d64381d29fe
paniz <paniz84yaghoobzadeh@gmail.com>
2024-07-06 00:56:18 +0330

    zemestan effect changed

---
+++ Zemestan.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include "Zemestan.hpp"
+int Zemestan::effectOfCard(std::vector<Card> cards, int score)
+{
+    std::cout<<"checkZemestanPlayed - >"<<checkZemestanPlayed<<std::endl;
+    if (checkZemestanPlayed)
+    {
+
+        score = cards.size() - countAllSpecialCards(cards);
+        std::cout << "score dar zemestan - >" << score << std::endl;
+    }
+    return score;
+}
+// void Zemestan::endOfZemestan()
+// {
+//     checkZemestanPlayed = false;
+// }
+// void Zemestan::zemestanPlayed()
+// {
+//     checkZemestanPlayed = true;
+// }
+int Zemestan ::countAllSpecialCards(std::vector<Card> cards)
+{
+    return countBahar(cards) + countMatarsak(cards) + countShir_dokht(cards) + countTabl_zan(cards) + countZemestan(cards) + countRish_sefid(cards) + countShirzan(cards) + countParcham_dar(cards);
+}
+int Zemestan ::countTabl_zan(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "tabl_zan")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countZemestan(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "zemestan")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countBahar(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "bahar")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countShir_dokht(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "shah_dokht")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countMatarsak(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "matarsak")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countShirzan(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "shirzan")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countRish_sefid(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "rish_sefid")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+int Zemestan ::countParcham_dar(std::vector<Card> cards)
+{
+    int counter = 0;
+    for (int j = 0; j < cards.size(); j++)
+    {
+        if (cards[j].getName() == "rish_sefid")
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
--- Zemestan.hpp
+++ Zemestan.hpp
@@ -1,13 +1,31 @@
 #ifndef ZEMESTAN_HPP
 #define ZEMESTAN_HPP
 #include "Special.hpp"
-class Zemestan : public Special
+class Zemestan :public Special
 {
-public:
-    virtual int effectOfCard(int score) override
-    {
-        return score = 1;
-    }
+    public:
+    virtual int effectOfCard(std::vector<Card>,int score)override;
+    //static void zemestanPlayed();
+    int countAllSpecialCards(std::vector<Card>);
+    int countZemestan(std::vector<Card>);
+    int countTabl_zan(std::vector<Card>);
+    int countBahar(std::vector<Card>);
+    int countShir_dokht(std::vector<Card>);
+    int countMatarsak(std::vector<Card>);
+    int countShirzan(std::vector<Card>);
+    int countRish_sefid(std::vector<Card>);
+    int countParcham_dar(std::vector<Card>);
+   // static void endOfZemestan();
+    static void endOfZemestan()
+{
+    checkZemestanPlayed = false;
+}
+static void zemestanPlayed()
+{
+    checkZemestanPlayed = true;
+}
+    static bool checkZemestanPlayed;
+    private:
 };
-
-#endif
+bool Zemestan::checkZemestanPlayed  (false);
+#endif 
