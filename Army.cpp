3efb71d5a8723d00a855aecaeab993af806c6e1e
paniz <paniz84yaghoobzadeh@gmail.com>
2024-07-05 19:38:14 +0330

    deleting useless functions in army

--- Army.cpp
+++ Army.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Army.hpp"
-// armyCard::armyCard(std::string name) : Card (name){};
 int armyCard::increasScore(std::string str, int score)
 {
     setCardScores();
--- Army.hpp
+++ Army.hpp
@@ -4,14 +4,6 @@
 class armyCard : public Card
 {
 public:
-    void setCharacter(int value) override
-    {
-        Card::setCharacter(value);
-    }
-    int getCharacter() override
-    {
-        return Card::getCharacter();
-    }
     int increasScore(std::string, int score);
 };
 #endif
