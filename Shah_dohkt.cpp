21185bfb517d721ef4c284681f894c4b78d03f10
paniz <paniz84yaghoobzadeh@gmail.com>
2024-07-06 12:05:52 +0330

    shah dokht effect updated

--- Shah_dohkt.cpp
+++ Shah_dohkt.cpp
@@ -1,11 +1,13 @@
+#include<iostream>
 #include"Shah_dokht.hpp"
 int Shah_dokht::effectOfCard(std::vector<Card> cards, int score)
 {
     for (int i = 0; i < cards.size(); i++)
     {
-        if (cards[i].getName() == "shah_dohkt")
+        if (cards[i].getName() == "shah_dokht")
         {
             score+=10;
         }
     }
+    return score;
 }
