2c1614e91cb554ed4cef436717b87621b949aecf
paniz <paniz84yaghoobzadeh@gmail.com>
2024-07-27 20:16:27 +0330

    function for handling input added

--- src/uiInput.cpp
+++ src/uiInput.cpp
@@ -7,20 +7,27 @@ char uiInput::getUIPlayerName(int index)
 {
     return UIPlayerName[index];
 }
-std::vector <char> uiInput::getAllUIPlayerNames()
+std::vector<char> uiInput::getAllUIPlayerNames()
 {
     return allUIPlayerNames;
 }
 void uiInput::initializeTextBoxRec()
 {
-    textBoxRec = {300,200,250,50};
+    textBoxRec = {300, 200, 250, 50};
 }
-void uiInput::setUIPlayerName(int index,char playerName)
+void uiInput::setUIPlayerName(int index, char playerName)
 {
     UIPlayerName[index] = playerName;
 }
 
-char* uiInput::getFullName()
+char *uiInput::getFullName()
 {
     return UIPlayerName;
+}
+void uiInput::resetInput()
+{
+    for (int i = 0; i < 50; i++)
+    {
+        UIPlayerName[i] = '\0';
+    }
 }
